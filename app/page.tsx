15a801a7360e781e95dc57eb6527ee264ddefe70
Alex <87208475+K0uzer@users.noreply.github.com>
2024-11-05 15:39:10 +0300

    feat: Add shadcn and your button

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my website</h1>
+      <Button variant='outline'>Кнопка</Button>
+    </div>
+  );
+}
