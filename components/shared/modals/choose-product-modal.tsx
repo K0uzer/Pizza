896fd10a153cbca71890ed63a0ec9dd584668523
Alex <87208475+K0uzer@users.noreply.github.com>
2024-11-28 21:46:12 +0300

    feat: modal window opening without data of product

---
+++ components/shared/modals/choose-product-modal.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { FC } from 'react'
+import { useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
+import { Product } from '@prisma/client'
+
+import { Dialog, DialogContent } from '@/components/ui/dialog'
+import { Title } from '../title'
+
+interface ChooseProductModalProps {
+    className?: string
+    product: Product
+}
+
+const ChooseProductModal: FC<ChooseProductModalProps> = ({
+    className,
+    product,
+}) => {
+    const router = useRouter()
+
+    return (
+        <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
+            <DialogContent
+                className={cn(
+                    'p-0 w-[1060px] min-h-[500px] bg-white overflow-hidden',
+                    className,
+                )}
+            >
+                <Title text={product.name} />
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export { ChooseProductModal }
